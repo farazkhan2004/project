3d62c1ffa2037fc4422e6bfe2be12a21cbe5767c
Aliyanahmed88 <aliyanahmed833@gmail.com>
2024-11-28 17:39:55 +0500

    first commit

---
+++ explore.js
@@ -0,0 +1,193 @@
+
+function pop() {
+  alert('submit success')
+}
+function other() {
+  alert('All Pakistan dailing Faraz:03432834042')
+}
+function buttom() {
+  alert('Wait For Updata')
+}
+// fordexplore                    fordexplore
+function a() {
+  var b = document.getElementById("im")
+  b.src = "ford-endeavour-to-raptor-1.jpg (1).webp"
+}
+function b() {
+  var b = document.getElementById("im")
+  b.src = "f150-lightning-matte-black (1).png";
+}
+function c() {
+  var b = document.getElementById("im")
+  b.src = "ford f150 4__800x450.jpg";
+}
+function d() {
+  var b = document.getElementById("im")
+  b.src ="Frank_F150_2_1500x.webp";
+}
+function e() {
+  var b = document.getElementById("im")
+  b.src = "6 (1).png";
+}
+function f() {
+  var b = document.getElementById("im")
+  b.src = "image-23 (1).png";
+}
+function g() {
+  var b = document.getElementById("im")
+  b.src = "LPI-PalmFord-June2024-Engines-LPI-PalmFord-F150Driving-SB3-1024x533.jpg";
+}
+function red() {
+  var b = document.getElementById("im")
+  b.src = "2020-Ford-F-150-Hero-Image .png";
+}
+function black() {
+  var b = document.getElementById("im")
+  b.src = "images (1).jpg";
+}
+function blue() {
+  var b = document.getElementById("im")
+  b.src = "images.jpg";
+}
+
+// honda explore                           honda explore
+
+function a1() {
+  var b = document.getElementById("ima")
+  b.src = "honda-hr-v-black-edition-dashboard.webp"
+}
+function b1() {
+  var b = document.getElementById("ima")
+  b.src = "ff0d8dd5-d880-c263-411c-6a4f73ebfe25.avif";
+}
+function c1() {
+  var b = document.getElementById("ima")
+  b.src = "car_0414_372400.jpg";
+}
+function d1() {
+  var b = document.getElementById("ima")
+  b.src = "sddefault.jpg";
+}
+function e1() {
+  var b = document.getElementById("ima")
+  b.src = "2019-honda-hr-v-rs-black-interior-malaysia-4.jpg";
+}
+function f1() {
+  var b = document.getElementById("ima")
+  b.src = "images (2).jpg";
+}
+function g1() {
+  var b = document.getElementById("ima")
+  b.src = "maxresdefault12.jpg";
+}
+
+
+function red1() {
+  var b = document.getElementById("ima")
+  b.src = "2022-Honda-HR-V-Sport.webp";
+}
+function black1() {
+  var b = document.getElementById("ima")
+  b.src = "images (2).jpg";
+}
+function blue1() {
+  var b = document.getElementById("ima")
+  b.src = "honda_hr_v_sport.jpg";
+}
+
+// suzuki explore                      suzuki explore
+
+function a3() {
+  var b = document.getElementById("imag")
+  b.src = "switzerland-geneva-march-suzuki-swift-front-view-th-international-motor-show-to-154109010 (1).png"
+}
+function b3() {
+  var b = document.getElementById("imag")
+  b.src = "suzuki-cultus-color-393735.avif";
+}
+function c3() {
+  var b = document.getElementById("imag")
+  b.src = "dashboard4 (1).png";
+}
+function d3() {
+  var b = document.getElementById("imag")
+  b.src = "Speedy-Blue-Metallic-Custom-Suzuki-Swift-Rear.jpg";
+}
+function e3() {
+  var b = document.getElementById("imag")
+  b.src = "new-blue-suzuki-swift-parked-roadside-side-view-udine-italy-june-new-blue-suzuki-swift-parked-roadside-side-view-249667452 (1).png";
+}
+function f3() {
+  var b = document.getElementById("imag")
+  b.src = "13556706983_1f0929098f_h.jpg";
+}
+function g3() {
+  var b = document.getElementById("imag")
+  b.src = "9d06f047a5ac4602a88be98cd2dbf718.3.webp";
+}
+
+
+function red3() {
+  var b = document.getElementById("imag")
+  b.src = "b9331adb0d7b5f78a02bfeda06cf70ca.png";
+}
+function black3() {
+  var b = document.getElementById("imag")
+  b.src = "bc41b1ef4e343f4650dac699e1c19a80.jpg";
+}
+function blue3() {
+  var b = document.getElementById("imag")
+  b.src = "swift-sport.png";
+}
+
+// fo video                          for video
+
+  // JavaScript to toggle video on play button click
+  document.getElementById('playButton').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor behavior
+
+    const carImage = document.getElementById('carImage');
+    const carVideo = document.getElementById('carVideo');
+    const playButton = document.getElementById('playButton');
+
+    // Hide the image and play button, show the video
+    carImage.style.display = 'none';
+    playButton.style.display = 'none';
+    carVideo.style.display = 'block';
+
+    // Start playing the video
+    carVideo.play();
+  });
+  
+  document.getElementById('playButton2').addEventListener('click', function (y) {
+    y.preventDefault(); // Prevent default anchor behavior
+
+    const carImage13 = document.getElementById('carImage13');
+    const carVideo13 = document.getElementById('carVideo13');
+    const playButton2 = document.getElementById('playButton2');
+
+    // Hide the image and play button, show the video
+    carImage13.style.display = 'none';
+    playButton2.style.display = 'none';
+    carVideo13.style.display = 'block';
+
+    // Start playing the video
+     carVideo13.play();
+  });
+
+  document.getElementById('playButton1').addEventListener('click', function (z) {
+    z.preventDefault(); // Prevent default anchor behavior
+
+    const carImage12 = document.getElementById('carImage12');
+    const carVideo12 = document.getElementById('carVideo12');
+    const playButton1 = document.getElementById('playButton1');
+
+    // Hide the image and play button, show the video
+    carImage12.style.display = 'none';
+    playButton1.style.display = 'none';
+    carVideo12.style.display = 'block';
+
+    // Start playing the video
+     carVideo12.play();
+  });
+
